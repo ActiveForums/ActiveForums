19e717b89409496307666ace3ce104935ef2c047
Ravi Gupta <ravigupta7986@gmail.com>
2014-10-15 23:03:18 +0530

    fixed build issue

--- controls/af_post.ascx.cs
+++ controls/af_post.ascx.cs
@@ -994,8 +994,6 @@ namespace DotNetNuke.Modules.ActiveForums
                             {
                                 amas.AddForumItemToJournal(PortalId, ForumModuleId, UserId, "forumtopic", sUrl, subject, body);
                             }
-
-                            sUrl = ctlUtils.BuildUrl(ForumTabId, ForumModuleId, ForumInfo.ForumGroup.PrefixURL, ForumInfo.PrefixURL, 0, 0, -1, "", -1, -1, "notread", 1, SocialGroupId);
                         }
                         catch (Exception ex)
                         {
