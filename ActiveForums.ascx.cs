c37f75fded18e19ede075299b3029d8a9bc749ef
artfuld <jocke@laserdome.tk>
2014-11-08 10:47:36 +0100

    My settings
    Cleaning up

--- ActiveForums.ascx.cs
+++ ActiveForums.ascx.cs
@@ -52,8 +52,7 @@ namespace DotNetNuke.Modules.ActiveForums
         {
             base.OnLoad(e);
 
-            // If the forum instance is on the user profile page, load the users prefs control
-            if (Request.QueryString["afgt"] != null && Request.QueryString["afgt"] == "afprofile")
+            if (Request.QueryString["afgt"] == "afprofile")
             {
                 int userId;
 
