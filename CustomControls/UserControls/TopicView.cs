85037b6f14a271ee7d1686b6af6e92f7a73b8b56
unknown <jagjoaand@JAG-STH-WKS-588.jag.se>
2015-03-16 16:45:44 +0100

    Cleaning up.

--- CustomControls/UserControls/TopicView.cs
+++ CustomControls/UserControls/TopicView.cs
@@ -1202,9 +1202,8 @@ namespace DotNetNuke.Modules.ActiveForums.Controls
             DotNetNuke.Entities.Users.UserController uc = new DotNetNuke.Entities.Users.UserController();
             DotNetNuke.Entities.Users.UserInfo author = uc.GetUser(DotNetNuke.Entities.Portals.PortalController.GetCurrentPortalSettings().PortalId, authorId);
 
-            // Populate the user object with the post author info if the author still exists.  
-            var up = new User();
-            up = new User
+            // Populate the user object with the post author info.  
+            var up = new User
             {
                 UserId = authorId,
                 UserName = username,
