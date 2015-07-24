acd66ca7368854ce7942a8a058ef4f0942a7bfb0
Seth Folickman <seth@asaptek.net>
2015-07-24 15:01:54 -0400

    Updated text editor js to help with IE issues
    Updated text_editor.js to help with Internet Explorer issues (inserting smilies and attachments etc).  Changes provided by Mitch Sellers (thank you Mitch !! )

--- scripts/text_editor.js
+++ scripts/text_editor.js
@@ -1,19 +1,9 @@
-﻿window.isTextEditor = true;
+window.isTextEditor = true;
 
 function amaf_insertHTML(html) {
-    if (document.getElementById(afeditor).createTextRange) {
-        document.getElementById(afeditor).focus(document.getElementById(afeditor).caretPos);
-        document.getElementById(afeditor).caretPos = document.selection.createRange().duplicate();
-        if (document.getElementById(afeditor).caretPos.text.length > 0) {
-            document.getElementById(afeditor).caretPos.text = document.getElementById(afeditor).caretPos.text;
-        } else {
-            document.getElementById(afeditor).caretPos.text = html;
-        };
-    } else {
     document.getElementById(afeditor).value += html;
-    };
-
 };
+
 function amaf_getBody() {
     return document.getElementById(afeditor).value;
 };
